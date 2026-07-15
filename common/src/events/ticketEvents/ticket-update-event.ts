8651a89fa874e7fa0bc117e88a1078b0f724bc26
Muhammad Ali Khan <ak098boy@gmail.com>
2026-07-15 21:05:01 +0500

    'update'

--- common/src/events/ticketEvents/ticket-update-event.ts
+++ common/src/events/ticketEvents/ticket-update-event.ts
@@ -8,5 +8,6 @@ export interface TicketUpdatedEvent {
     title: string;
     price: number;
     userId: string;
+    orderId?: string; // Optional field to store the order ID if the ticket is reserved
   };
 }
